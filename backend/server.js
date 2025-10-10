146812783ed28bf8ab63ae5c9f1cf78c2515189f
Ismail Nyms <smailkindle4@gmail.com>
2025-10-11 00:54:54 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,32 @@
-import express from 'express'
-import cors from  'cors'
-import 'dotenv/config'
-import connectDB from './config/mongoDb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRoute from './routes/userRoute.js'
+import { createServer } from 'vercel-node-server';
+import express from 'express';
+import cors from 'cors';
+import userRoute from './routes/userRoute.js';
 import translateRoutes from "./routes/translateRoutes.js";
 import timeRoutes from "./routes/time.js";
-import './jobs/emailReminder.js'
+import './jobs/emailReminder.js';
+import { connectDB, connectCloudinary } from './config.js';
 
-//app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const app = express();
 
-//middlewares
-app.use(express.json())
+// connect to DB and Cloudinary
+connectDB();
+connectCloudinary();
 
+app.use(express.json());
 app.use(cors({
-  origin: [
-    "https://booking-appointment-systeme-mern-st-iota.vercel.app", // رابط الـ frontend
-  ],
+  origin: ["https://booking-appointment-systeme-mern-st-iota.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true // ضروري لو تستخدم الكوكيز أو الـ Authorization header
+  credentials: true
 }));
 
-// السماح للمتصفح بتنفيذ طلبات OPTIONS (preflight)
 app.options("*", cors());
 
-//API endpoints
-app.use('/api/admin', adminRouter)
-app.use('/api/doctor', doctorRouter)
+// routes
 app.use('/api/user', userRoute)
 app.use("/api/translate", translateRoutes);
 app.use("/api/time", timeRoutes);
 
-app.get('/', (req,res)=>{
-    res.send("Woorking")
+app.get('/', (req,res)=> res.send("Working"));
 
-})
-
-export default app;
+export default createServer(app);
