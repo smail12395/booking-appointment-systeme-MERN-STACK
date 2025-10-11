4285563031d15d9904df3097af251b3201285c6f
Ismail Nyms <smailkindle4@gmail.com>
2025-10-11 09:50:29 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,37 @@
-import { createServer } from 'vercel-node-server';
-import express from 'express';
-import cors from 'cors';
-import userRoute from './routes/userRoute.js';
+import express from 'express'
+import cors from  'cors'
+import 'dotenv/config'
+import connectDB from './config/mongoDb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRoute from './routes/userRoute.js'
 import translateRoutes from "./routes/translateRoutes.js";
 import timeRoutes from "./routes/time.js";
-import './jobs/emailReminder.js';
-import { connectDB, connectCloudinary } from './config.js';
+import './jobs/emailReminder.js'
 
-const app = express();
+//app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
-// connect to DB and Cloudinary
-connectDB();
-connectCloudinary();
+//middlewares
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
-app.use(cors({
-  origin: ["https://booking-appointment-systeme-mern-st-iota.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
-
-app.options("*", cors());
-
-// routes
+//API endpoints
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRoute)
 app.use("/api/translate", translateRoutes);
 app.use("/api/time", timeRoutes);
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.get('/', (req,res)=>{
+    res.send("Woorking")
 
-app.get('/', (req,res)=> res.status(200).send("Backend Working 🚀"));
+})
 
-export default createServer(app);
+app.listen(port,() =>{
+    console.log('lestining at', port)
+})
